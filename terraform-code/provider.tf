d548f471479178bfe271e9c21a73cbb83deff363
andersonfesilva <anderson.ffelixsilva@gmail.com>
2024-11-25 22:38:36 -0300

    code to cta test

--- terraform-code/provider.tf
+++ terraform-code/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
   project = "corded-fragment-442201-r2" # Substitua pelo ID do seu projeto GCP
   region  = "us-central1"    # Região do bucket
-  credentials = jsondecode(file("key-cta-user.json"))
+  credentials = jsondecode(file("/var/jenkins_home/workspace/pipe-cta/terraform-code/key-cta-user.json"))
 }
