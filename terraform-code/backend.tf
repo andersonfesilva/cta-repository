61226468378f3301d78b608e7b5971ae4735baa2
andersonfesilva <anderson.ffelixsilva@gmail.com>
2024-11-27 14:14:26 -0300

    code to cta test

--- terraform-code/backend.tf
+++ terraform-code/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
-    #bucket  = "cta-tf-states" 
-    #prefix  = "env/prod"                 
+    bucket  = "cta-tf-states" 
+    prefix  = "env/prod"                 
     #project = "corded-fragment-442201-r2"
     #credentials = file(var.gcp_credentials_file)
   }
